05a950b65841ac93751c93b990eaf61265225397
Christian Stout <christian.allen.stout@gmail.com>
2026-01-05 23:29:10 -0500

    complete overhaul of the frontend html and css

--- src/web/main.js
+++ src/web/main.js
@@ -1,56 +1,65 @@
 import { get_tokens, highlight_text } from "../../pkg/lc3_emulator.js";
 
+const select = document.getElementById("themeSelect");
+document.body.dataset.theme = select.value; // to make sure that the default is loaded on init
+select.addEventListener("change", () => {
+  document.body.dataset.theme = select.value;
+});
+
 /*
 The following three functions and corresponding html & css are from:
 https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/
 */
 
-/**
- * replaces text in a textarea with text in a <code> tag, formatting, and highlighting
- *
- * @param {string} text
- * @returns {}
- */
-function update(text) {
-    let result_element = document.querySelector("#highlighted-content");
+// /**
+//  * replaces text in a textarea with text in a <code> tag, formatting, and highlighting
+//  *
+//  * @param {string} text
+//  * @returns {}
+//  */
+// function update(text) {
+//   let result_element = document.querySelector("#highlighted-content");
 
-    if(text[text.length-1] == "\n") {
-        text += " ";
-    }
+//   if (text[text.length - 1] == "\n") {
+//     text += " ";
+//   }
 
-    text = text.replace(new RegExp("&", "g"), "&").replace(new RegExp("<", "g"), "<");
-    
-    result_element.innerHTML = highlight_text(text);
-}
+//   text = text
+//     .replace(new RegExp("&", "g"), "&")
+//     .replace(new RegExp("<", "g"), "<");
 
-/**
- * replaces text in a textarea with text in a <code> tag, formatting, and highlighting
- *
- * @param {HTMLTextAreaElement} element
- * @returns {}
- */
-function sync_scroll(element) {
-    console.log(`SYNC_SCROLL: ${element}`);
-    let result_element = document.querySelector("#highlighting");
+//   result_element.innerHTML = highlight_text(text);
+// }
 
-    result_element.scrollTop = element.scrollTop;
-    result_element.scrollLeft = element.scrollLeft;
-}
+// /**
+//  * replaces text in a textarea with text in a <code> tag, formatting, and highlighting
+//  *
+//  * @param {HTMLTextAreaElement} element
+//  * @returns {}
+//  */
+// function sync_scroll(element) {
+//     console.log(`SYNC_SCROLL: ${element}`);
+//     let result_element = document.querySelector("#highlighting");
 
-function check_tab(element, event) {
-    console.log(`CHECK_TAB: ${element}, ${event}`);
-    let code = element.value;
-    if(event.key == "Tab") {
-        event.preventDefault(); // stop normal
-        let before_tab = code.slice(0, element.selectionStart); // text before tab
-        let after_tab = code.slice(element.selectionEnd, element.value.length); // text after tab
-        let cursor_pos = element.selectionEnd + 1; // where cursor moves after tab - moving forward by 1 char to after tab
-        element.value = before_tab + "\t" + after_tab; // add tab char
-        // move cursor
-        element.selectionStart = cursor_pos;
-        element.selectionEnd = cursor_pos;
-        update(element.value); // Update text to include indent
-    }
-}
+//     result_element.scrollTop = element.scrollTop;
+//     result_element.scrollLeft = element.scrollLeft;
+// }
 
-export {highlight_text, update, sync_scroll, check_tab}
+// function check_tab(element, event) {
+//   console.log(`CHECK_TAB: ${element}, ${event}`);
+//   let code = element.value;
+//   if (event.key == "Tab") {
+//     event.preventDefault(); // stop normal
+//     let before_tab = code.slice(0, element.selectionStart); // text before tab
+//     let after_tab = code.slice(element.selectionEnd, element.value.length); // text after tab
+//     let cursor_pos = element.selectionEnd + 1; // where cursor moves after tab - moving forward by 1 char to after tab
+//     element.value = before_tab + "\t" + after_tab; // add tab char
+//     // move cursor
+//     element.selectionStart = cursor_pos;
+//     element.selectionEnd = cursor_pos;
+//     update(element.value); // Update text to include indent
+//   }
+// }
+
+// export { highlight_text, update, sync_scroll, check_tab };
+// export { update };
